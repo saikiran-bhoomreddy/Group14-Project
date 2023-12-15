2b402833f2421bcc0f3c39c36f6dbe8d9ff19335
saikiran-bhoomreddy <55652283+saikiran-bhoomreddy@users.noreply.github.com>
2023-12-14 21:33:49 -0500

    Updated Code With Suggested Changes
    ->Added Graphs and Improved the Accuracy of the model with More number of data sets

--- Saikiran(RQ-2).py
+++ Saikiran(RQ-2).py
@@ -1,11 +1,10 @@
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 # Load data
-df = pd.read_csv("C:\\Users\\bhomr\\Desktop\\Software Engineering\\output.csv")  # File Path
-
+df = pd.read_csv('/content/output.csv')  # Replace with your actual file path
 
 # Explore data
 print(df.head())
@@ -15,17 +14,43 @@ df['Prompt'].fillna('', inplace=True)
 
 # Preprocess the data
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(df['Prompt'])
-y = df['WordCount']
+XP = vectorizer.fit_transform(df['Prompt'])
+YP = df['WordCount']
 
-# Training the model
+X,y=XP[:100],YP[:100]
+xtest,ytest=XP[100:],YP[100:]
+# Train the model
 model = LinearRegression()
 model.fit(X, y)
 
-# Example usage
-new_prompt = ["give me summary about heart functioning ", np.nan]  # Include NaN values in new prompts
-new_prompt_cleaned = ['' if pd.isna(prompt) else prompt for prompt in new_prompt]
-new_prompt_vectorized = vectorizer.transform(new_prompt_cleaned)
+# Example usage with a new prompt
+new_prompt = ["World with single gender "]
+new_prompt_vectorized = vectorizer.transform(new_prompt)
 predicted_word_count = model.predict(new_prompt_vectorized)
 
-print(f"Predicted Word Count: {predicted_word_count}")
+print(f"Expected Word Count for New Prompt: {predicted_word_count[0]}")
+
+# Plot actual vs predicted word count for the test set
+ypred = []
+for x in xtest:
+  ypred.append(model.predict(x)[0])
+
+ytest = list(ytest)
+import matplotlib.pyplot as plt
+
+# Plotting the black line
+plt.plot(ypred, color='black', label='Predicted')
+
+# Plotting the red line
+plt.plot(ytest, color='red', label='Test')
+
+# Adding labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Predicted Vs Test')
+
+# Adding a legend
+plt.legend()
+
+# Display the plot
+plt.show()
